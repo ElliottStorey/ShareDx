c977901fda07db390f2db87449b2cdde27cccc80
Glitch (glitch-hello-react) <none>
2022-11-19 16:30:57 +0000

    ⛄️🚈 Checkpoint ./src/pages/login.jsx:51259090/4931

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,23 +4,44 @@ import { Link } from "wouter";
 import "../styles/styles.css";
 
 import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Button,
+} from "@chakra-ui/react";
+
+import {
   FormControl,
   FormLabel,
   FormErrorMessage,
   FormHelperText,
   Input,
   Center,
-  Flex
-} from "@chakra-ui/react";
+  Flex,
+} from '@chakra-ui/react';
 
 export default function Login() {
   return (
-    <Flex justify='center' align='center'>
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
+    <Flex height='100%' grow='1' direction='row' align='center' justify='center'>
+      <Card>
+        <CardHeader>
+          <Heading>Fomite Login</Heading>
+        </CardHeader>
+        <CardBody>
+          <FormControl>
+            
+            <FormLabel>Username</FormLabel>
+            <Input type='email' />
+            <FormLabel>Password</FormLabel>
+            <Input type='password' />
+            <Center>
+              <Button margin='20px' >Login</Button>
+            </Center>
+          </FormControl>
+        </CardBody>
+      </Card>
     </Flex>
   );
 }
